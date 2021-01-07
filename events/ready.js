b29d2dc366fd7342b455b56b886bbebe8f57e1c9
David Studen <59510723+SoulyZero@users.noreply.github.com>
2021-01-07 12:50:26 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,11 +2,7 @@ module.exports = (client) => {
     console.log(`Logged in as ${client.user.tag}`)
     client.user.setPresence({
         activity: {
-<<<<<<< HEAD
             name: 'PogOS!',
-=======
-            name: 'PogOS',
->>>>>>> master
             type: 'PLAYING'
         }
     });
