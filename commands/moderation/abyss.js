146ff35d760e9c6745b7e204aa03a59f08430c48
David <studen.david@gmail.com>
2021-01-28 08:46:34 +0100

    added persistent abyss

--- commands/moderation/abyss.js
+++ commands/moderation/abyss.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require('discord.js');
+const fs = require('fs');
 
 module.exports = {
     name: 'abyss',
@@ -15,33 +16,59 @@ module.exports = {
         } else {
             const user = message.mentions.users.first();
 
-            if (user) {
+            if (message.member.guild.roles.cache.find(role => role.name === "ABYSS")) {
+                if (user) {
 
-                const member = message.guild.members.resolve(user);
+                    const member = message.guild.members.resolve(user);
 
-                if (member) {
+                    if (member) {
 
-                    member.roles.add(message.guild.roles.cache.find(r => r.name === "ABYSS")).catch(err => console.error(err));
-                    const embed = new MessageEmbed()
-                        .setColor(process.env.COLOR)
-                        .setTitle(`${user.tag} is now in the abyss!`)
+                        fs.readFile('./persistentRoles/persistentAbyss.txt', function (err1, dupe) {
+                            if (err1) throw err1;
+                            if (dupe.indexOf(user.id) >= 0) {
+                                const embed = new MessageEmbed()
+                                    .setColor(process.env.COLOR)
+                                    .setTitle(`${user.tag} is already in the abyss.`)
 
-                    message.channel.send(embed).then(m => m.delete({ timeout: 10000 })).catch(err => console.error(err));
+                                return message.channel.send(embed).then(m => m.delete({ timeout: 10000 })).catch(err => console.error(err));
+                            }
+                            else {
+                                fs.appendFile('./persistentRoles/persistentAbyss.txt', user.id + `,\n`, function (err2) {
+                                    if (err2) throw err2;
+                                    console.log('Updated the abyss file!');
+                                });
 
+                                member.roles.add(message.guild.roles.cache.find(r => r.name === "ABYSS")).catch(err => console.error(err));
+                                const embed = new MessageEmbed()
+                                    .setColor(process.env.COLOR)
+                                    .setTitle(`${user.tag} is now in the abyss!`)
+
+                                message.channel.send(embed).then(m => m.delete({ timeout: 10000 })).catch(err => console.error(err));
+                            }
+                        });
+
+                    } else {
+                        const embed = new MessageEmbed()
+                            .setColor(process.env.COLOR)
+                            .setTitle(`Unknown user!`)
+
+                        message.channel.send(embed).then(m => m.delete({ timeout: 10000 })).catch(err => console.error(err));
+                    }
                 } else {
                     const embed = new MessageEmbed()
                         .setColor(process.env.COLOR)
-                        .setTitle(`Unknown user!`)
+                        .setTitle(`No users were mentioned!`)
 
                     message.channel.send(embed).then(m => m.delete({ timeout: 10000 })).catch(err => console.error(err));
+
                 }
+
             } else {
                 const embed = new MessageEmbed()
                     .setColor(process.env.COLOR)
-                    .setTitle(`No users were mentioned!`)
+                    .setTitle(`Please generate the ABYSS role first!`)
 
                 message.channel.send(embed).then(m => m.delete({ timeout: 10000 })).catch(err => console.error(err));
-
             }
         }
     }
--- commands/moderation/surface.js
+++ commands/moderation/surface.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require('discord.js');
+const fs = require('fs');
 
 module.exports = {
     name: 'surface',
@@ -27,6 +28,30 @@ module.exports = {
                         .setTitle(`${user.tag} is no longer in the abyss!`)
 
                     message.channel.send(embed).then(m => m.delete({ timeout: 10000 })).catch(err => console.error(err));
+
+                    fs.readFile('./persistentRoles/persistentAbyss.txt', function (err, data) {
+                        if (err) throw error;
+                        data = data + '';
+                        let dataArray = data.split('\n');
+                        const searchKeyword = user.id;
+                        let lastIndex = -1;
+
+                        for (let index = 0; index < dataArray.length; index++) {
+                            if (dataArray[index].includes(searchKeyword)) {
+                                lastIndex = index;
+                                break;
+                            }
+                        }
+
+                        dataArray.splice(lastIndex, 1);
+
+                        const updatedData = dataArray.join('\n');
+                        fs.writeFile('./persistentRoles/persistentAbyss.txt', updatedData, (err) => {
+                            if (err) throw err;
+                            console.log('Successfully removed from the abyss data');
+                        });
+
+                    });
 
                 } else {
                     const embed = new MessageEmbed()
