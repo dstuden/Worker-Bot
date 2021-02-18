4a336b575978db14104af1ea40c47b9dd67ca877
David Studen <59510723+SoulyZero@users.noreply.github.com>
2021-02-18 17:05:04 +0100

    Update leave.js

--- commands/moderation/leave.js
+++ commands/moderation/leave.js
@@ -8,12 +8,15 @@ module.exports = {
     run: async (client, message) => {
         message.delete();
         if (message.member.hasPermission('MANAGE_GUILD' || 'KICK_MEMBERS')) {
+            const embed = new MessageEmbed()
+                .setColor(process.env.COLOR)
+                .setTitle('I have left the server. Sadge...')
+            message.channel.send(embed).then(m => m.delete({ timeout: 10000 })).catch(err => console.error(err));
             message.guild.leave();
         } else {
             const embed = new MessageEmbed()
                 .setColor(process.env.COLOR)
                 .setTitle('You dont have the permissions to do that!')
-
             message.channel.send(embed).then(m => m.delete({ timeout: 10000 })).catch(err => console.error(err));
         }
     }
