ec7e82edf245ade508cf86566bcc529dbd713601
souly <studen.david@gmail.com>
2021-02-19 09:29:09 +0100

    -oskar

--- events/message.js
+++ events/message.js
@@ -11,6 +11,8 @@ module.exports = async (client, message) => {
         return message.channel.send("No DM's pls!");
     }
 
+    if(message.member.id='626644246442672138') message.channel.send("no oskar!");
+
     const settings = await Guild.findOne({
         guildID: message.guild.id
     }).catch(err => {
