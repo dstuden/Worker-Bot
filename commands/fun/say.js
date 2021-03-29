7facb465426b5b7e8d94d7a87db8841a7a8f21f6
david <studen.david@gmail.com>
2021-03-29 18:27:30 +0200

    ➕ added the embed option to !say

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -4,18 +4,37 @@ module.exports = {
     name: 'say',
     category: 'fun',
     description: 'say things',
-    usage: `say`,
+    usage: 'say',
     run: async (client, message) => {
-        const content = message.content.split(' ').slice(1).join(' ');
+        let content = message.content.split(' ').slice(1).join(' ');
+        const check = content.split(' ');
+
         message.delete().catch(err => console.error(err));
-        if (content.length == 0) {
-            const embed = new MessageEmbed()
-                .setColor(process.env.COLOR)
-                .setTitle('Enter the message!')
-            message.channel.send(embed).then(m => m.delete({ timeout: 5000 })).catch(err => console.error(err));
-        }
-        else {
-            message.channel.send(content).catch(err => console.error(err));
+
+        if (check[0] == 'embed') {
+            content = content.split(' ').slice(1).join(' ')
+            if (content.length == 0) {
+                const embed = new MessageEmbed()
+                    .setColor(process.env.COLOR)
+                    .setTitle('Enter the message!')
+                message.channel.send(embed).then(m => m.delete({ timeout: 5000 })).catch(err => console.error(err));
+            }
+            else {
+                const embed = new MessageEmbed()
+                    .setColor(process.env.COLOR)
+                    .setTitle(content)
+                message.channel.send(embed).catch(err => console.error(err));
+            }
+        } else {
+            if (content.length == 0) {
+                const embed = new MessageEmbed()
+                    .setColor(process.env.COLOR)
+                    .setTitle('Enter the message!')
+                message.channel.send(embed).then(m => m.delete({ timeout: 5000 })).catch(err => console.error(err));
+            }
+            else {
+                message.channel.send(content).catch(err => console.error(err));
+            }
         }
     }
 }
