e8d73cc0e87dcc8f22446265ad6d2a2f249c21c7
David Studen <59510723+dstuden@users.noreply.github.com>
2022-01-08 19:42:41 +0100

    chika

--- commands/fun/chika.js
+++ commands/fun/chika.js
@@ -2,23 +2,23 @@ const { MessageEmbed } = require('discord.js');
 const { fetch } = require('../../utils/fetch');
 
 module.exports = {
-    name: 'foob',
+    name: 'chika',
     category: 'fun',
-    description: 'Send a random image of Fubuki Shirakami',
-    usage: `foob`,
+    description: 'Send a random image of Chika Fujiwara',
+    usage: `chika`,
     run: async (client, message) => {
 
         const url = `post/index.json?limit=1&tags=${message?.channel?.nsfw ? '' : 'rating:s+'}+order:random+fujiwara_chika`; // we do a little trolling pepeLa
         const response = JSON.parse(await fetch(url))[0];
         const embed = new MessageEmbed().setColor('#d9639a')
         if (response?.file_url) {
-            /(webp|jpg|jpeg|png)/.test(response.file_url)? embed.setImage(response.file_url) : embed.attachFiles([{ name: 'foob.png', attachment: response.file_url }])
+            /(webp|jpg|jpeg|png)/.test(response.file_url)? embed.setImage(response.file_url) : embed.attachFiles([{ name: 'chika.png', attachment: response.file_url }])
         } else {
-            embed.setImage(foob404)
+            embed.setImage(chika404)
         }
         
         message.channel.send(embed).catch(err => console.error(err));
     }
 }
 
-const foob404 = 'https://media.discordapp.net/attachments/909421773987127309/921834614669058088/foob_404.png'
+const chika404 = 'https://media.discordapp.net/attachments/909421773987127309/921834614669058088/chika_404.png'
