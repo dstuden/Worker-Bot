65b193817184e72cafc397d1ff3ff38f421ddc52
david <studen.david@gmail.com>
2022-01-09 15:26:20 +0100

    ffs martin moment

---
+++ commands/fun/chika.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require('discord.js');
+const { fetch } = require('../../utils/fetch');
+
+module.exports = {
+    name: 'chika',
+    category: 'fun',
+    description: 'Send a random image of Fujiwara Chika',
+    usage: `chika`,
+    run: async (client, message) => {
+
+        const url = `post/index.json?limit=1&tags=${message?.channel?.nsfw ? '' : 'rating:s+'}+order:random+fujiwara_chika`;
+        const response = JSON.parse(await fetch(url))[0];
+        const embed = new MessageEmbed().setColor(process.env.COLOR)
+        if (response?.file_url) {
+            /(webp|jpg|jpeg|png)/.test(response.file_url)? embed.setImage(response.file_url) : embed.attachFiles([{ name: 'chika.png', attachment: response.file_url }])
+        } else {
+            embed.setImage(chika404)
+            embed.setTitle("Whoops! **404**")
+        }
+
+        message.channel.send(embed).catch(err => console.error(err));
+    }
+}
+
+const chika404 = 'https://c.tenor.com/1IxBeCZ8uNgAAAAC/uzaki-chika.gif';
